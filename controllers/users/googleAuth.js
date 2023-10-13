7604b54dae016b24d0b59f14a27b590edceeae78
Dmytro Petyshyn <dev6012@gmail.com>
2023-10-13 16:51:20 +0300

    fix

--- controllers/users/googleAuth.js
+++ controllers/users/googleAuth.js
@@ -5,8 +5,8 @@ const googleAuth = async (req, res) => {
     client_id: process.env.GOOGLE_CLIENT_ID,
     redirect_uri: `${process.env.BASE_URL}/api/auth/google-redirect`,
     scope: [
-      'https://www.googleapis.com/auth/userifo.email',
-      'https://www.googleapis.com/auth/userifo.profile',
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/userinfo.profile',
     ].join(' '),
     response_type: 'code',
     access_type: 'offline',
