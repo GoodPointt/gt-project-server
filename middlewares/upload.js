78cd24d4ea6e8a84b9bd9bbe3774eca96d71979a
NikKms <mykola.kamyshnikov@gmail.com>
2023-10-15 12:06:57 +0300

    add change password, cloudanary

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,36 +1,45 @@
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
-const path = require('path');
 
-const { HttpError } = require('../utils');
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
 
-const tempDir = path.join(__dirname, '../', 'temp');
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: async (req, file) => {
+    const { _id } = req.user
+    if (!file) {
+      return {};
+    }
 
-const multerConfig = multer.diskStorage({
-  destination: tempDir,
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    let folder;
+    if (file.fieldname === 'avatarURL') {
+      folder = 'avatars';
+    }
+
+    const transformation = {
+      width: 250,
+      height: 250,
+      crop: 'fill',
+    };
+
+    const filename = file.originalname.split(".")[0]
+
+    return {
+      folder: folder,
+      allowed_formats: ['.png', '.jpg', '.webp', 'jpeg'],
+      public_id: `${_id}_${filename}`,
+      transformation: [transformation],
+    };
   },
 });
 
 const upload = multer({
-  storage: multerConfig,
-  limits: {
-    fieldNameSize: 300,
-    fileSize: 1000000,
-  },
-  fileFilter: (req, file, callback) => {
-    const acceptableExtensions = ['.png', '.jpg', '.webp', 'jpeg'];
-    if (!acceptableExtensions.includes(path.extname(file.originalname))) {
-      return callback(HttpError(400, 'Wrong file type'));
-    }
-
-    const fileSize = parseInt(req.headers['content-length']);
-    if (fileSize > 1000000) {
-      return callback(HttpError(400, 'File size too large'));
-    }
-
-    callback(null, true);
-  },
+  storage
 });
 
 module.exports = upload;
